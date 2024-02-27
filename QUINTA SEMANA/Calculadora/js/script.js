24df8b6cc8195490fe4f2a665989e83b53416f80
David Cárdenas <santito500@live.com>
2024-02-27 11:42:24 -0500

    Calculadora v4

--- QUINTA SEMANA/Calculadora/js/script.js
+++ QUINTA SEMANA/Calculadora/js/script.js
@@ -89,6 +89,76 @@ const display = {
       this.nchars--;
     }
   },
+  operation: function (value, equals = false) {
+    if (this.content.value === "") return;
+    let result = eval(this.content.value);
+    const functions = ["sqrt", "square", "inverse"];
+    if (
+      !calculator.memory.isEmpty() ||
+      typeof calculator.memory.peek() === "number"
+    ) {
+      const a = calculator.memory.peekBefore();
+      const b = eval(this.content.value);
+      let actualValue = "";
+      if (value === "equals") {
+        actualValue = "equals";
+      } else if (equals) {
+        actualValue = value;
+      } else {
+        actualValue = calculator.memory.peek();
+      }
+      switch (actualValue) {
+        case "plus":
+          result = calculator.add(a, b);
+          break;
+        case "minus":
+          result = calculator.subtract(a, b);
+          break;
+        case "multiply":
+          result = calculator.multiply(a, b);
+          break;
+        case "division":
+          result = calculator.divide(a, b);
+          break;
+        case "percentage":
+          result = calculator.percentage(a, b);
+          break;
+        case "equals":
+          const value = calculator.memory.peek();
+          if (typeof value === "string") {
+            this.operation(calculator.memory.peek(), true);
+          }
+          calculator.memory.clear();
+          return;
+      }
+    }
+    if (functions.includes(value)) {
+      const a = eval(this.content.value);
+      switch (value) {
+        case "inverse":
+          result = calculator.inverse(a);
+          break;
+        case "sqrt":
+          result = calculator.sqrt(a);
+          break;
+        case "square":
+          result = calculator.square(a);
+          break;
+      }
+    }
+    if (value !== "equals") {
+      if (String(result).length > 10) {
+        this.content.value = String(result.toExponential(3));
+        console.log(result);
+        return;
+      }
+      this.content.value = result;
+    }
+    calculator.memory.push(eval(this.content.value));
+    calculator.memory.push(value);
+    calculator.memory.newNumber = true;
+    console.log(result);
+  },
 };
 
 function addValueDisplay(value) {
@@ -108,71 +178,6 @@ function clearDisplay() {
   display.clear();
 }
 
-function operationValueDisplay(value, equals = false) {
-  if (display.content.value === "") return;
-  let result = eval(display.content.value);
-  const functions = ["sqrt", "square", "inverse"];
-  if (
-    !calculator.memory.isEmpty() ||
-    typeof calculator.memory.peek() === "number"
-  ) {
-    const a = calculator.memory.peekBefore();
-    const b = eval(display.content.value);
-    let actualValue = "";
-    if (value === "equals") {
-      actualValue = "equals";
-    } else if (equals) {
-      actualValue = value;
-    } else {
-      actualValue = calculator.memory.peek();
-    }
-    switch (actualValue) {
-      case "plus":
-        result = calculator.add(a, b);
-        break;
-      case "minus":
-        result = calculator.subtract(a, b);
-        break;
-      case "multiply":
-        result = calculator.multiply(a, b);
-        break;
-      case "division":
-        result = calculator.divide(a, b);
-        break;
-      case "percentage":
-        result = calculator.percentage(a, b);
-        break;
-      case "equals":
-        const value = calculator.memory.peek();
-        if (typeof value === "string") {
-          operationValueDisplay(calculator.memory.peek(), true);
-        }
-        calculator.memory.clear();
-        return;
-    }
-  }
-  if (functions.includes(value)) {
-    const a = eval(display.content.value);
-    switch (value) {
-      case "inverse":
-        result = calculator.inverse(a);
-        break;
-      case "sqrt":
-        result = calculator.sqrt(a);
-        break;
-      case "square":
-        result = calculator.square(a);
-        break;
-    }
-  }
-  if (value !== "equals") {
-    if (String(result).length > 10) {
-      display.content.value = String(result.toExponential(3));
-      return;
-    }
-    display.content.value = result;
-  }
-  calculator.memory.push(eval(display.content.value));
-  calculator.memory.push(value);
-  calculator.memory.newNumber = true;
+function operationValueDisplay(value) {
+  display.operation(value);
 }
